b5dab4c6d794b6dd3bba8693b730859f90dde160
Dani RyR <rennisdaniel@gmail.com>
2026-06-24 00:21:45 +0000

    fix: invid_sync apunta a servidor local + CORS headers

--- invid_sync.php
+++ invid_sync.php
@@ -1,6 +1,9 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(204); exit; }
 
 define('INVID_BASE',       'https://invidcomputers.com/api');
 define('INVID_TOKEN_FILE', __DIR__ . '/invid_token.json');
